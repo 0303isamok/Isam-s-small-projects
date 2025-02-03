f6eb52f1b21b2f725fb76d281e798b1a814de4be
0303isamok <kartitisam@gmail.com>
2025-02-03 13:51:13 +0100

    RandomNumberGenerator
    update on the code

--- Dice/script.js
+++
@@ -1,5 +0,0 @@
-const randomNumber = () =>{
-    num = document.getElementById("num").value;
-    res = document.getElementById("result");
-    res.innerHTML = Math.ceil(Math.random()*num);
-}
---
+++ RandNumGen/script.js
@@ -0,0 +1,5 @@
+const randomNumber = () =>{
+    num = document.getElementById("num").value;
+    res = document.getElementById("result");
+    res.innerHTML = Math.ceil(Math.random()*num);
+}
